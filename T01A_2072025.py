cd157460da7e3a2c5368bed8a2fd11b4e0abeb00
Sherly Santiadi <71547739+sntdshrly@users.noreply.github.com>
2023-12-28 22:00:22 +0700

    pushh t01

---
+++ T01A_2072025.py
@@ -0,0 +1,6 @@
+def main():
+    print("Halo nama saya Sherly Santiadi dengan NRP 2072025")
+
+
+if __name__ == '__main__':
+    main()
---
+++ T01B_2072025.py
@@ -0,0 +1,9 @@
+def main():
+    alpukat, apel, jeruk = (
+        int(input("Jumlah Alpukat (kg) :")),
+        int(input("Jumlah Apel (kg) :")),
+        int(input("Jumlah Jeruk (kg) :")))
+    print(f"=====================\nTotal Harga Buah Rp {(40000*alpukat)+(35000*apel)+(20000*jeruk)}")
+
+if __name__ == '__main__':
+    main()
